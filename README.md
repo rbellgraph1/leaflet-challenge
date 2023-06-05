4fc483081f920f87748ef72c913d8bc9cf4480e4
rbellgraph1 <38970921+rbellgraph1@users.noreply.github.com>
2023-06-04 22:33:06 -0700

    Work, updated readme, html and css files

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # leaflet-challenge
+
+Resources:  AskBCS, Tutor, Leaflet site, W3 Schools, ChatGPT, OpenStreetMap, StackOverflow and Youtube. 
+
+Instructions
+The instructions for this activity are broken into two parts:
+
+Part 1: Create the Earthquake Visualization
+
+Part 2: Gather and Plot More Data (Optional with no extra points earning)
+
+Part 1: Create the Earthquake Visualization
+2-BasicMap
+
+Your first task is to visualize an earthquake dataset. Complete the following steps:
+
+Get your dataset. To do so, follow these steps:
+
+The USGS provides earthquake data in a number of different formats, updated every 5 minutes. Visit the USGS GeoJSON FeedLinks to an external site. page and choose a dataset to visualize. The following image is an example screenshot of what appears when you visit this link:
+3-Data
+
+When you click a dataset (such as "All Earthquakes from the Past 7 Days"), you will be given a JSON representation of that data. Use the URL of this JSON to pull in the data for the visualization. The following image is a sampling of earthquake data in JSON format:
+4-JSON
+
+Import and visualize the data by doing the following:
+
+Using Leaflet, create a map that plots all the earthquakes from your dataset based on their longitude and latitude.
+
+Your data markers should reflect the magnitude of the earthquake by their size and the depth of the earthquake by color. Earthquakes with higher magnitudes should appear larger, and earthquakes with greater depth should appear darker in color.
+
+Hint: The depth of the earth can be found as the third coordinate for each earthquake.
+
+Include popups that provide additional information about the earthquake when its associated marker is clicked.
+
+Create a legend that will provide context for your map data.
+
+Your visualization should look something like the preceding map.
+
+Requirements
+These requirements apply only to "Part 1: Create the Earthquake Visualization" as "Part 2" is optional with no extra points earning.
+
+Map (60 points)
+TileLayer loads without error (20 points)
+
+Connects to geojson API using D3 without error (20 points)
+
+Markers with size corresponding to earthquake magnitude (10 points)
+
+A legend showing the depth and their corresponding color (10 points)
+
+Data Points (40 points)
+Data points scale with magnitude level (10 points)
+
+Data points colors change with depth level (10 points)
+
+Each point has a tooltip with the Magnitude, the location and depth (10 points)
+
+All data points load in the correct locations (10 points)
---
+++ static/js/logic.js
@@ -0,0 +1,80 @@
+
+
+// Creating the map object
+var myMap = L.map("map").setView([37.0902, -95.7129], 5.45);
+
+// Adding the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+// Store our API endpoint as queryUrl.
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Perform a GET request to the query URL/
+d3.json(queryUrl).then(function (data) {
+    console.log(data);
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+            const properties = feature.properties;
+            const geometry = feature.geometry;
+            const coordinates = geometry.coordinates;
+            const magnitude = properties.mag;
+            const depth = coordinates[2];
+            const title = properties.title;
+            const type = properties.type;
+
+            // Calculate marker size based on magnitude
+            const markerSize = magnitude * 5;
+
+            // Calculate marker color based on depth
+            var markerColor = "";
+            if (depth <= 10) {
+                markerColor = "rgb(102, 255, 102)";
+            } else if (depth <= 30) {
+                markerColor = "rgb(255, 255, 102)";
+            } else if (depth <= 50) {
+                markerColor = "rgb(255, 217, 102)";
+            } else if (depth <= 70) {
+                markerColor = "rgb(255, 179, 102)";
+            } else if (depth <= 90) {
+                markerColor = "rgb(255, 140, 102)";
+            } else {
+                markerColor = "rgb(255, 102, 102)";
+            }
+
+            // Create the marker
+            return L.circleMarker(latlng, {
+                radius: markerSize,
+                color: markerColor,
+                fillColor: markerColor,
+                fillOpacity: 0.7, 
+                className: 'custom-circle-marker' // CSS class name
+            }).bindPopup(`Type: ${type}<br>Coordinates: ${coordinates}<br>Info: ${title}<br>Magnitude: ${depth}<br>`);
+        }
+    }).addTo(myMap);
+
+    // Create legend
+    var legend = L.control({ position: 'bottomright' });
+
+    legend.onAdd = function () {
+        var div = L.DomUtil.create('div', 'info legend');
+        var labels = ['-10-10', '11-30', '31-50', '51-70', '71-90', '90+'];
+        var colors = ["rgb(102, 255, 102)", "rgb(255, 255, 102)", "rgb(255, 217, 102)", "rgb(255, 179, 102)", "rgb(255, 140, 102)", "rgb(255, 102, 102)"];
+
+        for (var i = 0; i < labels.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + colors[i] + '"></i> ' +
+                labels[i] + '<br>';
+        }
+        return div;
+    };
+
+    legend.addTo(myMap);
+});
+
+
+
+
+
+
